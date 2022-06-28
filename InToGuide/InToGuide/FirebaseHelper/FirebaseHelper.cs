f76b19625580335a6531ab1723adb70bdfc90ad5
Ashley <4270837@myuwc.ac.za>
2022-06-28 13:09:28 +0200

    Classes added in Folders

---
+++ InToGuide/InToGuide/FirebaseHelper/FirebaseHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace InToGuide.FirebaseHelper
+{
+    public class FirebaseHelper : ContentPage
+    {
+        public FirebaseHelper()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
