6d72c7153dabf3b4547a0f47361a8c677b70655b
Ashley <4270837@myuwc.ac.za>
2022-06-28 13:07:18 +0200

    .

---
+++ InToGuide/InToGuide/Model/Form2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace InToGuide.Model
+{
+    public class Form2 : ContentPage
+    {
+        public Form2()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
